872f8102a6b312a9565bdddae9ce4e524fc4524e
wandji20 <wbertrand10@gmail.com>
2021-02-11 10:36:44 +0100

    Generate sessions model and add methods and views for the sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    user = User.find_by_userid(params[:userid]) || User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+
+end
