93c476ac5ceac1b20f7c2024b882d63dfdbf9309
wandji20 <wbertrand10@gmail.com>
2021-02-12 09:13:29 +0100

    Add attendance model and association between events and users

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :attendances, foreign_key: 'attended_event_id'
+  has_many :attendees, class_name: 'User', :through => :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :events
+  has_many :attendances, foreign_key: 'attendee_id'
+  has_many :attended_events, class_name: 'Event', :through => attendances
 end
